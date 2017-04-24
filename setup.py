a5f1fec68fec9658331cdf40257b639a882c003a
Nate Ricklin <nate@rickl.in>
2017-04-24 11:44:13 -0600

    initial scaffolding

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import sys
+from setuptools import setup, find_packages
+
+open_kwds = {}
+if sys.version_info > (3,):
+    open_kwds['encoding'] = 'utf-8'
+
+setup(name='leafpy',
+      version='0.0.1',
+      description='Lightweight python interface to the nissan leaf.',
+      classifiers=[],
+      keywords='',
+      author='Nate Ricklin',
+      author_email='nate.ricklin@gmail.com',
+      url='https://github.com/nricklin/leafpy',
+      license='MIT',
+      packages=find_packages(exclude=['docs','tests','examples']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['requests'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest','vcrpy']
+      )
