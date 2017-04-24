2432517e86b8ff36911a4edc850baed15dd4c3be
Nate Ricklin <nate@rickl.in>
2017-04-24 17:50:35 -0600

    finish callable getattr way of hitting the API

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # leafpy
 lightweight python interface to the nissan leaf
+
+
+# Get battery status:
+
+```python
+from leafpy import Leaf
+leaf = Leaf(username, password)
+
+# Tell the leaf to check itself for battery status
+result = leaf.BatteryStatusCheckRequest()
+
+# after waiting some amount of time for that to happen, get the battery status
+leaf.BatteryStatusCheckResultRequest(resultKey=result['resultKey'])
+
+# or just check whatever battery status is available without polling first:
+leaf.BatteryStatusRecordsRequest()
+```
--- leafpy/leaf.py
+++ leafpy/leaf.py
@@ -1,4 +1,7 @@
 from auth import login
+import requests
+
+BASE_URL = 'https://gdcportalgw.its-mo.com/gworchest_160803A/gdc/'
 
 class Leaf(object):
     """Make requests to the Nissan Connect API to get Leaf Info"""
@@ -7,11 +10,33 @@ class Leaf(object):
 
     def __init__(self, username=None, password=None, custom_sessionid=None, VIN=None):
 
-    	# If you instantiate with a username & password, login.
+        # If you instantiate with a username & password, login.
         if username and password:
-        	self.custom_sessionid, self.VIN = login(username, password)
+            self.custom_sessionid, self.VIN = login(username, password)
         elif custom_sessionid and VIN:
-        	self.custom_sessionid = custom_sessionid
-        	self.VIN = VIN
+            self.custom_sessionid = custom_sessionid
+            self.VIN = VIN
         else:
-        	raise Exception('Need either username & password or custom_sessionid & VIN.')
+            raise Exception('Need either username & password or custom_sessionid & VIN.')
+
+    def __getattr__(self, name):
+        """
+        All methods that don't require input data
+        """
+
+        def call(**kwargs):
+            url = BASE_URL + name + '.php'
+            data = {
+                "RegionCode": 'NNA',
+                "custom_sessionid": self.custom_sessionid,
+                "VIN": self.VIN
+            }
+            for k in kwargs:
+                data[k] = kwargs[k]
+            r = requests.post(url, data=data)
+            r.raise_for_status()
+            if not r.json()['status'] == 200:
+            	raise Exception('Error making request.  Perhaps the session has expired.')
+            return r.json()
+
+        return call
