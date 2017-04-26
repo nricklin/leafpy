e9da4dd8ecba0ce16a5f4891542f9f933c4def6e
Nate Ricklin <nate@rickl.in>
2017-04-25 19:50:17 -0600

    add unit tests

---
+++ tests/unit/test_api_calls.py
@@ -0,0 +1,38 @@
+import unittest
+from leafpy import Leaf
+import vcr, time
+
+VIN = 'dummyvin'
+custom_sessionid = 'dummy_csid'
+
+class APICallTests(unittest.TestCase):
+
+    @vcr.use_cassette('tests/unit/cassettes/test_call_with_no_params.yaml', 
+        filter_post_data_parameters=['VIN','custom_sessionid'])
+    def test_call_with_no_params(self):
+        leaf = Leaf(VIN=VIN, custom_sessionid=custom_sessionid)
+
+        result = leaf.BatteryStatusRecordsRequest()
+
+        assert result['status'] == 200
+
+    @vcr.use_cassette('tests/unit/cassettes/test_call_with_params.yaml', 
+        filter_post_data_parameters=['VIN','custom_sessionid'])
+    def test_call_with_params(self):
+        leaf = Leaf(VIN=VIN, custom_sessionid=custom_sessionid)
+
+        response = leaf.BatteryStatusCheckRequest()
+        assert response['status'] == 200
+        #time.sleep(140)   # only need to pause here when running against live service
+        response2 = leaf.BatteryStatusCheckResultRequest(resultKey=response['resultKey'])
+        assert response2['status'] == 200
+
+    @vcr.use_cassette('tests/unit/cassettes/test_call_to_nonexistent_endpoint.yaml', 
+        filter_post_data_parameters=['VIN','custom_sessionid'])
+    def test_call_to_nonexistent_endpoint(self):
+        leaf = Leaf(VIN=VIN, custom_sessionid=custom_sessionid)
+
+        with self.assertRaises(Exception):
+            response = leaf.this_doesnt_exist()
+
+    
---
+++ tests/unit/test_login.py
@@ -0,0 +1,53 @@
+import unittest
+from leafpy import Leaf
+import vcr
+
+USERNAME = 'dummyuser'
+PASSWORD = 'dummypass'
+
+class LoginTests(unittest.TestCase):
+
+    @vcr.use_cassette('tests/unit/cassettes/test_login.yaml', 
+        filter_post_data_parameters=['UserId','Password'])
+    def test_login(self):
+        leaf = Leaf(USERNAME, PASSWORD)
+
+        assert leaf.VIN == "vin123"
+        assert leaf.custom_sessionid == "csessid"
+
+    def test_login_with_custom_sessionid_and_vin(self):
+        leaf = Leaf(VIN='vin345', custom_sessionid='csid123')
+
+        assert leaf.VIN == 'vin345'
+        assert leaf.custom_sessionid == 'csid123'
+
+    @vcr.use_cassette('tests/unit/cassettes/test_exeption_raised_when_bad_credentials_passed.yaml', 
+        filter_post_data_parameters=['UserId','Password'])
+    def test_exeption_raised_when_bad_credentials_passed(self):
+        with self.assertRaises(Exception) as w:
+            leaf = Leaf('bad_email@domain.com','invalidpassword')
+
+    @vcr.use_cassette('tests/unit/cassettes/test_exception_raised_when_bad_vin_and_customsessionid_used.yaml', 
+        filter_post_data_parameters=['UserId','Password'])
+    def test_exception_raised_when_bad_vin_and_customsessionid_used(self):
+
+        leaf = Leaf(VIN='vin345',custom_sessionid='csid123')
+
+        with self.assertRaises(Exception) as w:
+            leaf.BatteryStatusRecordsRequest()
+
+    def test_login_with_only_username_raises_exception(self):
+        with self.assertRaises(Exception):
+            leaf = Leaf('username')
+
+    def test_login_with_only_VIN_raises_exception(self):
+        with self.assertRaises(Exception):
+            leaf = Leaf(VIN='vin123')
+
+    def test_login_with_only_custom_sessionid_raises_exception(self):
+        with self.assertRaises(Exception):
+            leaf = Leaf(custom_sessionid='vin123')
+
+    def test_login_with_no_args_raises_exception(self):
+        with self.assertRaises(Exception):
+            leaf = Leaf()
